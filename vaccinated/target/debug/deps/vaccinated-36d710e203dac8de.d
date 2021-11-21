405a72cdf5d5534f88d180425aa4a76a480ef462
osxgirl <osxgirl@mac.com>
2021-11-20 22:56:21 -0500

    solana

---
+++ vaccinated/target/debug/deps/vaccinated-36d710e203dac8de.d
@@ -0,0 +1,5 @@
+/Users/heather/Development/code/solana/vaccinated/target/debug/deps/vaccinated-36d710e203dac8de: src/main.rs
+
+/Users/heather/Development/code/solana/vaccinated/target/debug/deps/vaccinated-36d710e203dac8de.d: src/main.rs
+
+src/main.rs:
---
+++ vaccinated/target/debug/vaccinated.d
@@ -0,0 +1,1 @@
+/Users/heather/Development/code/solana/vaccinated/target/debug/vaccinated: /Users/heather/Development/code/solana/vaccinated/src/main.rs
